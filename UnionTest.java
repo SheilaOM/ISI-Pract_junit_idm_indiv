b57c29eab27f1c1e9cceee3430467d52e12faa19
SheilaOM <sheila.oliva14@gmail.com>
2017-11-22 19:27:18 +0100

    Test1: repeat element

---
+++ UnionTest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class UnionTest
+{
+	private Vector a, b;
+
+	@Before      // Set up - Called before every test method.
+	public void setUp()
+	{
+		a = new Vector(5);
+		b = new Vector(5);
+	}
+
+	@Test
+	public void repeatElement()
+	{
+		a.addElement(2);
+		a.addElement(3);
+		a.addElement(4);
+
+	    b.addElement(2);
+	    b.addElement(5);
+	    b.addElement(a);
+
+	    Vector c = new Vector(5);
+	    c.addElement(2);
+	    c.addElement(3);
+	    c.addElement(4);
+	    c.addElement(5);
+	    c.addElement(a);
+
+	    assertTrue("A same element is in both vectors", c.equals(Union.union(a, b)));
+	}
+}
