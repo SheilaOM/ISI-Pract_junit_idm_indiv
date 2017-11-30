961f7803d8a80245c0c14eb65187c896f2a63717
SheilaOM <sheila.oliva14@gmail.com>
2017-11-30 10:58:44 +0100

    Añadido comentario explicativo

--- UnionSetTest.java
+++ UnionSetTest.java
@@ -4,21 +4,25 @@ import java.util.*;
 
 public class UnionSetTest {
 
-    private Set a, b;
+	private Set a, b;
 
-    @Before      // Set up - Called before every test method.
-    public void setUp()
-    {
-        a = new HashSet();
-        b = new HashSet();
-    }
+	@Before      // Set up - Called before every test method.
+	public void setUp()
+	{
+		a = new HashSet();
+		b = new HashSet();
+	}
 
-    @Test (expected = NullPointerException.class)
-    public void nullSet()
-    {
-        a = null;
-        b.add(4);
-        Union.unionSet(a, b);
-    }
+	@Test (expected = NullPointerException.class)
+	public void nullSet()
+	{
+		a = null;
+		b.add(4);
+		Union.unionSet(a, b);
+	}
 
+	/*No veo necesario implementar Test para comprobar, si se repiten valores
+	 * en un Set, ya que ésto lo implementa la propia interfaz de Set, al igual que
+	 * permitir que un elemento de Set sea null, y que éste se repita.
+	 */
 }
